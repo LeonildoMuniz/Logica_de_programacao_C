a212c029caa4ead5bb36586f4c3b35fc6752d53c
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-11-16 21:23:54 -0300

    Update Ex_Triagem_Aula.cpp

--- Exercícios Diversos/Ex_Triagem_Aula.cpp
+++ Exercícios Diversos/Ex_Triagem_Aula.cpp
@@ -75,11 +75,33 @@ void triagem(){
 		printf("\nEncaminhe-se para ala de risco MEDIO");
 	}
 }
+FILE* abre_arquivo(char caminho[50],char modo[1]){
+	FILE *arquivo;
+	arquivo = fopen(caminho,modo);
+	if(arquivo==NULL){
+		printf("Não foi possivel abrir o arquivo");
+	}
+	return arquivo;
+}
+
+void fecha_arquivo(FILE *arquivo){
+	fclose(arquivo);
+}
+
 
 int main(){
 	setlocale(LC_ALL,"Portuguese");
+	FILE *arquivo;
 	cadastro_paciente();
 	coleta_sintomas();
 	triagem();
+	arquivo = fopen("atendimento.txt","a");
+	fprintf(arquivo,"\nCPF: %s\nNome: %s\nSexo: %s\nIdade: %d\nPontos: %d\n--------------------------------"
+	,paciente.cpf
+	,paciente.nome
+	,paciente.sexo
+	,paciente.idade
+	,total_pontos);
+	fclose(arquivo);
 	return 0;
 }
