d2e9ca0d20fb7a93ccb9366381052bef8bcdaadf
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-11-17 21:10:39 -0300

    Finalização do projeto

--- Exercícios Diversos/Controle_Estoque.cpp
+++ Exercícios Diversos/Controle_Estoque.cpp
@@ -16,14 +16,6 @@ struct produto material[100];
 
 void cadastro(){
 	int sair=0;
-
-	FILE *arquivo;
-	arquivo = fopen("estoque.txt","a");
-	if(arquivo==NULL){
-		printf("Não foi possivel abrir o arquivo");
-		exit(1);
-	}
-
 	
 	while(sair!=1){
 		
@@ -38,24 +30,20 @@ void cadastro(){
 		printf("\nDigite o valor unitario: ");
 		scanf("%f",&material[cont].unit_produto);
 		material[cont].vlr_total=material[cont].qtde_produto*material[cont].unit_produto;
-		printf("\nDigite 1 para sair / 0 - Novo produto\n Opção:  ");
+		system("cls");
+		printf("\nEscolha uma das opções\n 1 - Sair\n 0 - Novo código\n Opção:  ");
 		scanf("%d",&sair);
+		
 		if(sair!=0 and sair!=1){
 			while(sair!=0 and sair!=1){
 				system("cls");
-				printf("\nValor digitado invalido, digite novamente, 1 - Sair / 0 - Novo produto \nOpção:");
+				printf("\nValor digitado invalido\n 1 - Sair\n 0 - Novo código\n Opção:  ");
 				scanf("%d",&sair);
 			}
 		}
-		fprintf(arquivo,"\nCodigo: %d\nDescrição: %s\nQuantidade: %.2f\nValor unitario: %.2f\nValor total: %.2f\n--------------------------------"
-		,material[cont].cod_produto
-		,material[cont].nome_produto
-		,material[cont].qtde_produto
-		,material[cont].unit_produto
-		,material[cont].vlr_total);
 		cont+=1;
 	}
-	fclose(arquivo);
+
 }
 
 void listagem_todos(){
@@ -68,11 +56,12 @@ void listagem_todos(){
 			printf("\nQuantidade: %.2f",material[i].qtde_produto);
 			printf("\nUnitario: %.2f",material[i].unit_produto);
 			printf("\nValor total: %.2f",material[i].vlr_total);
-			printf("\n=================================");
+			printf("\n=================================\n");
 		}	
 	}else{
-		printf("Nenhum cadastro realizado");
-	}		
+		printf("Nenhum cadastro realizado\n\n");
+	}	
+	system("pause");	
 }
 
 void listagem_codigo(){
@@ -91,11 +80,12 @@ void listagem_codigo(){
 			printf("\n=================================");
 			cont2+=1;
 		}
-		else if(cont2==0){
-			system("cls");
-			printf("\nNenhum código foi encontrado");
-		}
 	}
+	if(cont2==0){
+		system("cls");
+		printf("Nenhum código foi encontrado\n\n");
+	}
+	system("pause");
 }
 
 void altera_codigo(){
@@ -105,25 +95,19 @@ void altera_codigo(){
 	printf("Digite código para alterar: ");
 	scanf("%d",&codigo);
 	for(i=0;i<cont;i++){
-		printf("\nCodigo encontrado!\nEntre com os dados a seguir para alterar\n");
 		if(codigo==material[i].cod_produto){
-			printf("\nCódigo atual: %d - Novo: ",material[i].cod_produto);
-			scanf("%d",&material[i].cod_produto);
-			printf("\nDescrição atual: %s Nova: ",material[i].nome_produto);
-			fflush(stdin);
-			gets(material[i].nome_produto);
+			printf("\nCodigo encontrado!\nEntre com os dados a seguir para alterar\n");
 			printf("\nQuantidade atual: %.2f Nova: ",material[i].qtde_produto);
 			scanf("%f",&material[i].qtde_produto);
-			printf("\nUnitario atual: %.2f Novo: ",material[i].unit_produto);
-			scanf("%f",&material[i].unit_produto);
 			material[i].vlr_total=material[i].qtde_produto*material[i].unit_produto;
 			cont2+=1;
 		}
-		else if(cont2==0){
-			system("cls");
-			printf("\nNenhum código foi encontrado");
-		}
 	}
+	if(cont2==0){
+		system("cls");
+		printf("Nenhum código foi encontrado\n\n");
+	}
+	system("pause");
 }
 
 void ler_arquivo(){
@@ -132,30 +116,88 @@ void ler_arquivo(){
   char *result;
   int i;
   arquivo = fopen("estoque.txt", "rt");
-  if (arquivo == NULL) 
-  {
-     printf("Problemas na abertura do arquivo\n");
-     return;
+  if (arquivo == NULL){
+    printf("Arquivo aberto ou não existe\n");
+    system("pause");
+    return;
   }
   i = 1;
-  while (!feof(arquivo))
-  {
+  system("cls");
+  while (!feof(arquivo)){
       result = fgets(linha, 100, arquivo); 
       if (result)
 	  printf("%s",linha);
       i++;
   }
+  printf("\n");
+  system("pause");
   fclose(arquivo);
+  
 } 
 
-		
+void gravar_arq(){
+	int i;
+	FILE *arquivo;
+	arquivo = fopen("estoque.txt","a");
+	
+	if(arquivo==NULL){
+		printf("Não foi possivel abrir o arquivo");
+		return;
+	}
+	for(i=0;i<cont;i++){
+		fprintf(arquivo,"\nCodigo: %d\nDescrição: %s\nQuantidade: %.2f\nValor unitario: %.2f\nValor total: %.2f\n--------------------------------"
+		,material[i].cod_produto
+		,material[i].nome_produto
+		,material[i].qtde_produto
+		,material[i].unit_produto
+		,material[i].vlr_total);		
+	}
+	fclose(arquivo);
+	exit(1);
+}
+
+int menu(){
+    int opcao=0;
+    printf("Escolha a opção:\n 1 - Cadastrar novo código\n 2 - Listar todos cadastros em memoria\n 3 - Consulta por código em memoria\n 4 - Altera código memoria\n 5 - Gravar arquivo de dados TXT e sair\n 6 - Mostrar dados arquivo TXT\n 7 - Encerrar aplicação\n\nOpção===> : ");
+    scanf("%d",&opcao);
+    return opcao;
+}
+
 
 int main(){
 	setlocale(LC_ALL,"Portuguese");
-	cadastro();
-	ler_arquivo();
-	//altera_codigo();
-	//listagem_todos();
-	//listagem_codigo();
+	
+    int opcao;
+    opcao=menu();
+    while(opcao != 7){ 
+    switch(opcao){
+    	case 1:
+			cadastro();
+    	break;
+    	case 2:
+			listagem_todos();
+    	break;
+	    case 3:
+			listagem_codigo();
+    	break;
+    	case 4:
+			altera_codigo();
+    	break;
+    	case 5:
+			gravar_arq();
+    	break;
+    	case 6:
+			ler_arquivo();
+    	break;
+    	
+    	default:
+    		system("cls");
+    		printf("Opção escolhida invalida!\n\n");
+    		system("pause");
+    } 
+    system("cls");
+    opcao=menu(); 
+    }
+return 0;
 	
 }
